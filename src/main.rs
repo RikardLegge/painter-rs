df7be93c750fb4fd228c8a18dce61435995a5f53
Rikard Legge <rikard.legge@gmail.com>
2017-01-08 14:38:13 +0100

    Migrate graphics to use piston

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,95 @@
-#[macro_use]
-extern crate gfx;
-extern crate gfx_window_glutin;
-extern crate glutin;
-extern crate cgmath;
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
 
-use cgmath::{Vector2, Vector4, Matrix4, SquareMatrix, ortho};
-use gfx::traits::FactoryExt;
-use gfx::Device;
+use std::f64::consts;
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{ GlGraphics, OpenGL };
 
-pub type ColorFormat = gfx::format::Rgba8;
-pub type DepthFormat = gfx::format::DepthStencil;
+struct Point {
+    x: i32,
+    y: i32
+}
 
-gfx_defines!{
-    vertex Vertex {
-        pos: [f32; 2] = "a_Pos",
-        color: [f32; 3] = "a_Color",
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+    rotation: f64,   // Rotation for the square.
+    position: Point,   // Rotation for the square
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs, window: &Window) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 100.0);
+        let rotation = self.rotation;
+        let x = -self.position.x as f64 + 400.0;
+        let y = -self.position.y as f64 + 400.0;
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(GREEN, gl);
+
+            let transform = c.transform
+                .trans(x, y)
+                .rot_rad(rotation)
+                .trans(-50.0, -50.0);
+
+            // Draw a box rotating around the middle of the screen.
+            rectangle(RED, square, transform, gl);
+        });
+
+        window.window.set_position(self.position.x, self.position.y);
     }
 
-    pipeline pipe {
-        mvp: gfx::Global<[[f32; 4]; 4]> = "u_ModelViewProj",
-        vbuf: gfx::VertexBuffer<Vertex> = (),
-        out: gfx::RenderTarget<ColorFormat> = "Target0",
+    fn update(&mut self, args: &UpdateArgs) {
+        // Rotate 2 radians per second.
+        self.rotation += consts::PI * args.dt;
+
+        let x = self.rotation.cos()*100.0;
+        let y = self.rotation.sin()*100.0;
+
+        self.position.x = x as i32 + 200;
+        self.position.y = y as i32 + 200;
     }
 }
 
-const TRIANGLE: [Vertex; 3] = [
-    Vertex { pos: [ -0.5, -0.5 ], color: [1.0, 0.0, 0.0] },
-    Vertex { pos: [  0.5, -0.5 ], color: [0.0, 1.0, 0.0] },
-    Vertex { pos: [  0.0,  0.5 ], color: [0.0, 0.0, 1.0] }
-];
+fn main() {
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
 
-const CLEAR_COLOR: [f32; 4] = [0.1, 0.2, 0.3, 1.0];
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new(
+        "spinning-square",
+        [400, 400]
+    )
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 
-pub fn main() {
-    let builder = glutin::WindowBuilder::new()
-        .with_title("Triangle example".to_string())
-        .with_dimensions(512, 512)
-        .with_vsync();
-    let (window, mut device, mut factory, main_color, mut main_depth) =
-        gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
-    let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
-    let pso = factory.create_pipeline_simple(
-        include_bytes!("../shader/triangle_150.vert"),
-        include_bytes!("../shader/triangle_150.frag"),
-        pipe::new()
-    ).unwrap();
-    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&TRIANGLE, ());
-
-    let mut data = pipe::Data {
-        vbuf: vertex_buffer,
-        out: main_color,
-        mvp: Matrix4::identity().into()
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+        position: Point {x: 0, y: 0}
     };
 
-    let mut pos = Vector2::<i32> {
-        x: 0,
-        y: 100
-    };
-
-    'main: loop {
-        // loop over events
-        for event in window.poll_events() {
-            match event {
-                glutin::Event::KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Escape)) |
-                glutin::Event::Closed => break 'main,
-                glutin::Event::Resized(_width, _height) => {
-                    gfx_window_glutin::update_views(&window, &mut data.out, &mut main_depth);
-                },
-                _ => {},
-            }
+    let mut events = window.events();
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            app.render(&r, &window);
         }
-        pos.y = (pos.y + 1) % 512;
-        window.set_position(pos.x, pos.y + 60);
 
-        data.mvp[0][3] = pos.x as f32 / 256.0;
-        data.mvp[1][3] = pos.y as f32 / 256.0;
-
-        // draw a frame
-        encoder.clear(&data.out, CLEAR_COLOR);
-        encoder.draw(&slice, &pso, &data);
-        encoder.flush(&mut device);
-        window.swap_buffers().unwrap();
-        device.cleanup();
+        if let Some(u) = e.update_args() {
+            app.update(&u);
+        }
     }
 }
